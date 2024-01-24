fb17ba7411b9da805f3f83c28f9fd72c1af0a1c1
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-01-25 07:17:12 +0800

    using dict instead of list for combination. also its now working.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ except:
         exit()
 
 import platform
+import os
 from pynput import keyboard
 
 user_system = platform.system()
@@ -19,9 +20,15 @@ current_keys_pressed = []
 pressed = False
 
 if user_system == "Windows":
-	com = [[162, 160, 65], [162, 160, 66], [162, 160, 67]]
-elif user_system == "Linux":
-	com = [[65507, 65505, 65], [65507, 65505, 66], [65507, 65505, 67]]
+    com = {
+        (162, 164, 84): "cmd",
+        (162, 164, 78): "notepad"
+    }
+elif user_system == "Linux":    # Kali VM
+    com = {
+        # still not updated, my vm is acting weird
+        (65507, 65513, 116): "konsole"
+    }
 
 def on_press(key):
     global current_keys_pressed
@@ -39,12 +46,24 @@ def on_press(key):
 
     if key_press not in current_keys_pressed:
         current_keys_pressed.append(key_press)
-        
-    for i in range(len(com)):
-        if current_keys_pressed == com[i] and not pressed:
-            print("combinations pressed", i + 1)
+
+    for key, value in com.items():
+        if tuple(current_keys_pressed) == key and not pressed:
+
+            # executes the command
+            os.system(value)
+
             pressed = True
             break
+        
+    # if key_press not in current_keys_pressed:
+    #     current_keys_pressed.append(key_press)
+        
+    # for i in range(len(com)):
+    #     if current_keys_pressed == com[i] and not pressed:
+    #         print("combinations pressed", i + 1)
+    #         pressed = True
+    #         break
 
 def on_release(key):
     global current_keys_pressed
@@ -54,15 +73,27 @@ def on_release(key):
         key_press = key.vk
     except AttributeError:
         key_press = key.value.vk
-      
-    for i in range(len(com)):
-        if current_keys_pressed == com[i]:
-            print("combinations released", i + 1)
+    
+
+    for key, value in com.items():
+        if tuple(current_keys_pressed) == key:
+
+            # do something when the combination is released
+
             pressed = False
             break
-
+    
     if key_press in current_keys_pressed:
         current_keys_pressed.remove(key_press)
+
+    # for i in range(len(com)):
+    #     if current_keys_pressed == com[i]:
+    #         print("combinations released", i + 1)
+    #         pressed = False
+    #         break
+
+    # if key_press in current_keys_pressed:
+    #     current_keys_pressed.remove(key_press)
     
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import os
-import platform
+a = [10, 20]
+b =  {
+    (10, 20): "c",
+    (20, 30): "d"
+}
 
-os.system("ls")
-print(platform.system())
+for i, j in b.items():
+    print(i, j)
+    if tuple(a) == i:
+        print("yes")
